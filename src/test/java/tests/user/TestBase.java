c8b9d2f67c9e22259e6ad9e14f67637cb6a806d0
Aleksei Perchukov <aleksei.perchukov@gmail.com>
2022-12-11 23:01:50 +0300

    Full refactoring of the code: - Static methods - Static test data - Test data - New dedicated jenkins job settings

--- src/test/java/tests/user/TestBase.java
+++ src/test/java/tests/user/TestBase.java
@@ -23,12 +23,12 @@ public class TestBase {
             capabilities.setCapability("enableVideo", true);
         }
         Configuration.browserCapabilities = capabilities;
-        Configuration.browser = System.getProperty("browser_name", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browser = System.getProperty("browser_name", "vnc");
+        Configuration.browserVersion = System.getProperty("browser_version", "chrome_108.0");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
         Configuration.pageLoadStrategy = "none";
 
-        Configuration.timeout = 30000;
+        Configuration.timeout = 60000;
         if(remote != null) {
             Configuration.remote = remote;
         }
