7b02dfd3de792e35a1f7d7cbcc71590f489f1fc4
Aleksei Perchukov <aleksei.perchukov@gmail.com>
2022-12-11 23:10:39 +0300

    Full refactoring of the code: - Static methods - Static test data - Test data - New dedicated jenkins job settings

--- src/test/java/tests/user/TestBase.java
+++ src/test/java/tests/user/TestBase.java
@@ -1,6 +1,7 @@
 package tests.user;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
