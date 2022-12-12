89cb28eb8fe9840727e655f016383c6b7f7e29fb
aleksei-perchukov <aleksei.perchukov@gmail.com>
2022-12-12 11:08:46 +0300

    - Fixed issue with baseURI = null - Refactored testdata variables - Fixed payment tests

--- src/test/java/tests/checkout/guest/CheckoutTestsDE.java
+++ src/test/java/tests/checkout/guest/CheckoutTestsDE.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
 import tests.checkout.TestBase;
 import tests.user.UserTestData;
 
